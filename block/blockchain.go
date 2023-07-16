87a72fabd894954accb60a7cb200b573b504723a
Adhikari <adrameshadhikari95@gmail.com>
2023-07-16 18:29:55 -0400

    sign transaction with private and public key and validate transaction

--- block/blockchain.go
+++ block/blockchain.go
@@ -112,6 +112,11 @@ func (bc *Blockchain) AddTransaction(sender string, recipent string, value float
 		return true
 	}
 	if bc.veryfyTransactionSignature(senderPublicKey, s, t) {
+		if bc.CalculateTotalAmount(sender) < value {
+			log.Println("ERROR: Not enough balence in a wallet")
+			return false
+
+		}
 		bc.transactionPool = append(bc.transactionPool, t)
 		return true
 	} else {
