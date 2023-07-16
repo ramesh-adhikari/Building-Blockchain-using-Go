dbdbaa8bf47df5de407565bf4c3615a2a4943e07
Adhikari <adrameshadhikari95@gmail.com>
2023-07-16 14:17:27 -0400

    added mining method

--- blockchain.go
+++ blockchain.go
@@ -9,7 +9,11 @@ import (
 	"time"
 )
 
-const MINING_DIFFICULTY = 3
+const (
+	MINING_DIFFICULTY = 3
+	MINING_SENDER     = "THE BLOCKCHAIN"
+	MINING_REWARD     = 0.1
+)
 
 type Block struct {
 	timestamp    int64
@@ -69,13 +73,15 @@ func (b *Block) MarshalJSON() ([]byte, error) {
 }
 
 type Blockchain struct {
-	transactionPool []*Transaction
-	chain           []*Block
+	transactionPool   []*Transaction
+	chain             []*Block
+	blockchainAddress string
 }
 
-func NewBlockchain() *Blockchain {
+func NewBlockchain(blockchainAddress string) *Blockchain {
 	b := &Block{}
 	bc := new(Blockchain)
+	bc.blockchainAddress = blockchainAddress
 	bc.CreateBlock(0, b.Hash())
 	return bc
 }
@@ -129,6 +135,15 @@ func (bc *Blockchain) ProofOfWork() int {
 	return nonce
 }
 
+func (bc *Blockchain) Mining() bool {
+	bc.AddTransaction(MINING_SENDER, bc.blockchainAddress, MINING_REWARD)
+	nonce := bc.ProofOfWork()
+	previousHash := bc.LastBlock().Hash()
+	bc.CreateBlock(nonce, previousHash)
+	log.Println("action=mining, status=success")
+	return true
+}
+
 func NewTransaction(sender string, recipent string, value float32) *Transaction {
 	return &Transaction{sender, recipent, value}
 }
@@ -157,11 +172,13 @@ func init() {
 }
 
 func main() {
-	blockChain := NewBlockchain()
+	myBlockchainAddress := "my_blockchain_address"
+	blockChain := NewBlockchain(myBlockchainAddress)
 	blockChain.Print()
 
 	blockChain.AddTransaction("A", "B", 10)
 	blockChain.AddTransaction("A", "B", 10)
+	blockChain.Mining()
 	previousHash := blockChain.LastBlock().Hash()
 	nonce := blockChain.ProofOfWork()
 	blockChain.CreateBlock(nonce, previousHash)
